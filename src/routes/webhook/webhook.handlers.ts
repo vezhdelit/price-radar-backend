53fe6af0d2a7d7d2bc401814d0693c0c73291b44
Vasyl Vezhdel <vezhdelit@gmail.com>
2025-06-01 22:57:37 +0300

    fix

--- src/routes/webhook/webhook.handlers.ts
+++ src/routes/webhook/webhook.handlers.ts
@@ -7,7 +7,7 @@ import type { ScrapedHtmlWebhookRoute } from "./webhook.routes";
 export const scrapedHtmlWebhook: AppRouteHandler<ScrapedHtmlWebhookRoute> = async (c) => {
   // Extract the request body
   const body = c.req.valid("json");
-  c.var.logger.info("Received webhook data:", body);
+  c.var.logger.info(`Received webhook data: ${JSON.stringify(body, null, 2)}`);
 
   return c.json(
     {
